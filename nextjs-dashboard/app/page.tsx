813ff8d6ceaedd814324784c017c93e0e8240fd3
Keigo Yamauchi <40446332+Ovaw@users.noreply.github.com>
2024-12-19 17:51:40 +0900

    stream LatestInvoices

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,3 +1,4 @@
+import CardWrapper from '@/app/ui/dashboard/cards';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
